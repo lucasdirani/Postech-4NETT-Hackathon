575d2ed48696c80accdf1dc62ad941eb17cb51eb
Lucas Dirani <lruizd@protonmail.com>
2025-02-02 16:08:38 -0300

    test(servicos): adicionando teste unitário para o método ValidarConflitoHorarioAgendamento da classe ServicoAgendamento, onde o novo agendamento possui uma sobreposição parcial no início do agendamento existente.

--- testes/Postech.Hackathon.Agendamentos.TestesUnitarios/Suite/Dominio/Servicos/ServicoAgendamentoTestes.cs
+++ testes/Postech.Hackathon.Agendamentos.TestesUnitarios/Suite/Dominio/Servicos/ServicoAgendamentoTestes.cs
@@ -115,4 +115,31 @@ public class ServicoAgendamentoTestes
         // Assert
         possuiConflito.Should().BeTrue();
     }
+
+    [Fact(DisplayName = "Novo agendamento com sobreposição parcial no início (conflito)")]
+    [Trait("Action", "ValidarConflitoHorarioAgendamento")]
+    public void ValidarConflitoHorarioAgendamento_NovoAgendamentoSobreposicaoParcialInicio_DeveRetornarVerdadeiro()
+    {
+        // Arrange
+        Guid idMedico = Guid.NewGuid();
+        DateOnly dataAtual = new(2025, 2, 1);
+        IReadOnlyList<Agendamento> agendamentos =
+        [
+            new(idMedico, dataAgendamento: new(2025, 2, 2), horarioInicioAgendamento: new(9, 0, 0), horarioFimAgendamento: new(9, 30, 0), dataAtual),
+            new(idMedico, dataAgendamento: new(2025, 2, 2), horarioInicioAgendamento: new(9, 30, 0), horarioFimAgendamento: new(10, 0, 0), dataAtual),
+            new(idMedico, dataAgendamento: new(2025, 2, 2), horarioInicioAgendamento: new(10, 0, 0), horarioFimAgendamento: new(10, 30, 0), dataAtual),
+            new(idMedico, dataAgendamento: new(2025, 2, 2), horarioInicioAgendamento: new(10, 30, 0), horarioFimAgendamento: new(11, 0, 0), dataAtual),
+            new(idMedico, dataAgendamento: new(2025, 2, 2), horarioInicioAgendamento: new(11, 0, 0), horarioFimAgendamento: new(11, 30, 0), dataAtual),
+            new(idMedico, dataAgendamento: new(2025, 2, 2), horarioInicioAgendamento: new(15, 0, 0), horarioFimAgendamento: new(15, 30, 0), dataAtual),
+            new(idMedico, dataAgendamento: new(2025, 2, 2), horarioInicioAgendamento: new(15, 30, 0), horarioFimAgendamento: new(16, 0, 0), dataAtual)
+        ];
+        TimeSpan horarioInicioNovoAgendamento = new(8, 45, 0);
+        TimeSpan horarioFimNovoAgendamento = new(9, 15, 0);
+
+        // Act
+        bool possuiConflito = _servico.ValidarConflitoHorarioAgendamento(agendamentos, horarioInicioNovoAgendamento, horarioFimNovoAgendamento);
+
+        // Assert
+        possuiConflito.Should().BeTrue();
+    }
 }
