c7fc85c18fc55cf46b410f82c236f2c9be307a25
Lucas Dirani <lruizd@protonmail.com>
2025-02-02 15:13:47 -0300

    test(entidades): adicionando teste unitário para o construtor da classe Agendamento, onde a data do agendamento é anterior a data atual.

--- src/Postech.Hackathon.Agendamentos.Dominio/Entidades/Agendamento.cs
+++ src/Postech.Hackathon.Agendamentos.Dominio/Entidades/Agendamento.cs
@@ -7,13 +7,17 @@ public class Agendamento(
     Guid idMedico,
     DateOnly dataAgendamento,
     TimeSpan horarioInicioAgendamento,
-    TimeSpan horarioFimAgendamento) : EntidadeBase
+    TimeSpan horarioFimAgendamento,
+    DateOnly dataAtual) : EntidadeBase
 {
     public Guid IdMedico { get; private set; } = idMedico.EstaVazio() 
         ? throw new ExcecaoDominio("O identificador do médico é obrigatório", nameof(Agendamento), nameof(IdMedico)) 
         : idMedico;
         
-    public DateOnly Data { get; private set; } = dataAgendamento;
+    public DateOnly Data { get; private set; } = dataAgendamento < dataAtual
+        ? throw new ExcecaoDominio("A data de agendamento deve ser igual ou maior do que a data atual", nameof(Agendamento), nameof(Data))
+        : dataAgendamento;
+
     public TimeSpan HorarioInicio { get; private set; } = horarioInicioAgendamento;
     public TimeSpan HorarioFim { get; private set; } = horarioFimAgendamento;
 }
--- testes/Postech.Hackathon.Agendamentos.TestesUnitarios/Suite/Dominio/Entidades/AgendamentoTestes.cs
+++ testes/Postech.Hackathon.Agendamentos.TestesUnitarios/Suite/Dominio/Entidades/AgendamentoTestes.cs
@@ -12,12 +12,13 @@ public class AgendamentoTestes
     {
         // Arrange
         Guid idMedico = Guid.NewGuid();
+        DateOnly dataAtual = new(2025, 2, 1);
         DateOnly dataAgendamento = new(2025, 2, 2);
         TimeSpan horarioInicioAgendamento = new(12, 0, 0);
         TimeSpan horarioFimAgendamento = new(12, 30, 0);
 
         // Act
-        Agendamento agendamento = new(idMedico, dataAgendamento, horarioInicioAgendamento, horarioFimAgendamento);
+        Agendamento agendamento = new(idMedico, dataAgendamento, horarioInicioAgendamento, horarioFimAgendamento, dataAtual);
 
         // Assert
         agendamento.Should().NotBeNull();
@@ -35,16 +36,37 @@ public class AgendamentoTestes
     {
         // Arrange
         Guid idMedico = Guid.Empty;
+        DateOnly dataAtual = new(2025, 2, 1);
         DateOnly dataAgendamento = new(2025, 2, 1);
         TimeSpan horarioInicioAgendamento = new(15, 0, 0);
         TimeSpan horarioFimAgendamento = new(15, 30, 0);
 
         // Act
-        ExcecaoDominio excecao = Assert.Throws<ExcecaoDominio>(() => new Agendamento(idMedico, dataAgendamento, horarioInicioAgendamento, horarioFimAgendamento));
+        ExcecaoDominio excecao = Assert.Throws<ExcecaoDominio>(() => new Agendamento(idMedico, dataAgendamento, horarioInicioAgendamento, horarioFimAgendamento, dataAtual));
 
         // Assert
         excecao.Mensagem.Should().NotBeNullOrEmpty();
         excecao.Acao.Should().Be(nameof(Agendamento));
         excecao.Propriedade.Should().Be(nameof(Agendamento.IdMedico));
+    }
+
+    [Fact(DisplayName = "Data do agendamento anterior a data atual")]
+    [Trait("Action", "Agendamento")]
+    public void Agendamento_DataAgendamentoAnteriorDataAtual_DeveLancarExcecaoDeDominio()
+    {
+        // Arrange
+        Guid idMedico = Guid.NewGuid();
+        DateOnly dataAtual = new(2025, 2, 3);
+        DateOnly dataAgendamento = new(2025, 2, 1);
+        TimeSpan horarioInicioAgendamento = new(15, 0, 0);
+        TimeSpan horarioFimAgendamento = new(15, 30, 0);
+
+        // Act
+        ExcecaoDominio excecao = Assert.Throws<ExcecaoDominio>(() => new Agendamento(idMedico, dataAgendamento, horarioInicioAgendamento, horarioFimAgendamento, dataAtual));
+
+        // Assert
+        excecao.Mensagem.Should().NotBeNullOrEmpty();
+        excecao.Acao.Should().Be(nameof(Agendamento));
+        excecao.Propriedade.Should().Be(nameof(Agendamento.Data));
     }
 }
