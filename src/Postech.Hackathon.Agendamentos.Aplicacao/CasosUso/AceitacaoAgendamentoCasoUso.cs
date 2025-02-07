1dea7cce04ce58f5628d17d7759f8e53d7d85f01
Lucas Dirani <lruizd@protonmail.com>
2025-02-07 15:55:17 -0300

    test(casos de uso): adicionando teste unitário para o método ExecutarAsync da classe AceitacaoAgendamentoCasoUso, onde o agendamento é aceito após a sua data original.

--- src/Postech.Hackathon.Agendamentos.Aplicacao/CasosUso/AceitacaoAgendamentoCasoUso.cs
+++ src/Postech.Hackathon.Agendamentos.Aplicacao/CasosUso/AceitacaoAgendamentoCasoUso.cs
@@ -3,6 +3,7 @@ using Postech.Hackathon.Agendamentos.Aplicacao.CasosUso.Enumeradores;
 using Postech.Hackathon.Agendamentos.Aplicacao.CasosUso.Interfaces;
 using Postech.Hackathon.Agendamentos.Aplicacao.CasosUso.Saidas;
 using Postech.Hackathon.Agendamentos.Dominio.Entidades;
+using Postech.Hackathon.Agendamentos.Dominio.Excecoes.Comum;
 using Postech.Hackathon.Agendamentos.Dominio.Repositorios;
 
 namespace Postech.Hackathon.Agendamentos.Aplicacao.CasosUso;
@@ -13,12 +14,19 @@ public class AceitacaoAgendamentoCasoUso(IRepositorioAgendamento repositorio) : 
 
     public async Task<AceitacaoAgendamentoSaida> ExecutarAsync(AceitacaoAgendamentoEntrada entrada)
     {
-        Agendamento? agendamento = await _repositorio.ObterPorIdAsync(entrada.IdAgendamento);
-        if (agendamento is null) return new() { SituacaoAceitacaoAgendamento = SituacaoAceitacaoAgendamento.AgendamentoNaoEncontrado, Mensagem = "Agendamento inexistente" };
-        if (!agendamento.PertenceMedico(entrada.IdMedico)) return new() { SituacaoAceitacaoAgendamento = SituacaoAceitacaoAgendamento.AceitacaoNaoPermitida, Mensagem = "Aceitação não permitida" };
-        agendamento.AceitarAgendamento(entrada.DataAceitacao);
-        _repositorio.Atualizar(agendamento);
-        await _repositorio.SalvarAlteracoesAsync();
-        return new() { SituacaoAceitacaoAgendamento = SituacaoAceitacaoAgendamento.Sucesso };
+        try
+        {
+            Agendamento? agendamento = await _repositorio.ObterPorIdAsync(entrada.IdAgendamento);
+            if (agendamento is null) return new() { SituacaoAceitacaoAgendamento = SituacaoAceitacaoAgendamento.AgendamentoNaoEncontrado, Mensagem = "Agendamento inexistente" };
+            if (!agendamento.PertenceMedico(entrada.IdMedico)) return new() { SituacaoAceitacaoAgendamento = SituacaoAceitacaoAgendamento.AceitacaoNaoPermitida, Mensagem = "Aceitação não permitida" };
+            agendamento.AceitarAgendamento(entrada.DataAceitacao);
+            _repositorio.Atualizar(agendamento);
+            await _repositorio.SalvarAlteracoesAsync();
+            return new() { SituacaoAceitacaoAgendamento = SituacaoAceitacaoAgendamento.Sucesso };
+        }
+        catch (ExcecaoDominio ex)
+        {
+            return new() { SituacaoAceitacaoAgendamento = SituacaoAceitacaoAgendamento.DadosInvalidos, Mensagem = ex.Mensagem };
+        }
     }
 }
--- testes/Postech.Hackathon.Agendamentos.TestesUnitarios/Suite/Aplicacao/CasosUso/AceitacaoAgendamentoCasoUsoTestes.cs
+++ testes/Postech.Hackathon.Agendamentos.TestesUnitarios/Suite/Aplicacao/CasosUso/AceitacaoAgendamentoCasoUsoTestes.cs
@@ -106,4 +106,39 @@ public class AceitacaoAgendamentoCasoUsoTestes
         repositorio.Verify(r => r.ObterPorIdAsync(agendamentoSendoAceito.Id), Times.Once());
         repositorio.Verify(r => r.SalvarAlteracoesAsync(), Times.Never());
     }
+
+    [Fact(DisplayName = "Aceitar agendamento após a data cadastrada para o agendamento")]
+    [Trait("Action", "ExecutarAsync")]
+    public async Task ExecutarAsync_AceitarAgendamentoAposDataCadastradaAgendamento_NaoDeveAceitarAgendamento()
+    {
+        // Arrange
+        Guid idMedico = Guid.NewGuid();
+        Guid idPaciente = Guid.NewGuid();
+        DateOnly dataAgendamento = new(2025, 2, 7);
+        DateOnly dataEfetuacaoAgendamento = new(2025, 2, 6);
+        TimeSpan horarioInicioAgendamento = new(12, 0, 0);
+        TimeSpan horarioFimAgendamento = new(13, 0, 0);
+        DateOnly dataCadastro = new(2025, 2, 5);
+        decimal valorAgendamento = 100;
+        Agendamento agendamentoSendoAceito = new(idMedico, dataAgendamento, horarioInicioAgendamento, horarioFimAgendamento, dataCadastro, valorAgendamento);
+        agendamentoSendoAceito.EfetuarAgendamento(idPaciente, dataEfetuacaoAgendamento);
+        Mock<IRepositorioAgendamento> repositorio = new();  
+        repositorio.Setup(r => r.ObterPorIdAsync(agendamentoSendoAceito.Id)).ReturnsAsync(() => agendamentoSendoAceito);
+        AceitacaoAgendamentoEntrada entrada = new()
+        {
+            IdAgendamento = agendamentoSendoAceito.Id,
+            IdMedico = idMedico,
+            DataAceitacao = new DateOnly(2025, 2, 8)
+        };
+        AceitacaoAgendamentoCasoUso casoUso = new(repositorio.Object);
+
+        // Act
+        AceitacaoAgendamentoSaida saida = await casoUso.ExecutarAsync(entrada);
+
+        // Assert
+        saida.SituacaoAceitacaoAgendamento.Should().Be(SituacaoAceitacaoAgendamento.DadosInvalidos);
+        saida.Mensagem.Should().NotBeNullOrEmpty();
+        repositorio.Verify(r => r.ObterPorIdAsync(agendamentoSendoAceito.Id), Times.Once());
+        repositorio.Verify(r => r.SalvarAlteracoesAsync(), Times.Never());
+    }
 }
