30077cb4955ed2b104ad99f1c2368630db1bedff
Lucas Dirani <lruizd@protonmail.com>
2025-02-08 14:52:55 -0300

    feat(health check): adicionando endpoint de health check na API.

--- src/Postech.Hackathon.Agendamentos.Api/Program.cs
+++ src/Postech.Hackathon.Agendamentos.Api/Program.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Postech.Hackathon.Agendamentos.Api.HealthChecks;
 using Postech.Hackathon.Agendamentos.Api.Setup;
 using Postech.Hackathon.Agendamentos.Infra.Controladores.Http;
 using Postech.Hackathon.Agendamentos.Infra.Http.Adaptadores;
@@ -8,7 +10,6 @@ builder.WebHost.UseUrls("http://*:5060");
 builder.Configuration.AdicionarArquivoJsonPeloAmbiente(builder.Environment.EnvironmentName);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AdicionarDependenciaRefit(builder.Configuration);
 builder.Services.AdicionarDependenciaNotificador();
 builder.Services.AdicionarDependenciaServicoInfra();
@@ -16,8 +17,15 @@ builder.Services.AdicionarDependenciaServicoDominio();
 builder.Services.AdicionarDbContext(builder.Configuration);
 builder.Services.AdicionarDependenciaRepositorio();
 builder.Services.AdicionarDependenciaCasoUso();
+builder.Services.AddHealthChecks().AdicionarHealthCheckPostgres(builder.Configuration);
 
 WebApplication app = builder.Build();
+
+app.MapHealthChecks("/health"); 
+app.MapHealthChecks("/ready", new HealthCheckOptions
+{
+    Predicate = check => check.Tags.Contains("ready")
+});
 
 app.UseHttpsRedirection();
 app.UsarMiddlewareValidacaoToken();
